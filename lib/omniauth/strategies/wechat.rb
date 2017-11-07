ddfd6690710b05b6227ee3a113ce06c7d326638c
Hongtao Qi <hongtaoqi@HongtaodeMacBook-Pro.local>
2017-11-07 17:38:24 +0800

    if fix_redirect_uri nil use call_back_uri

--- lib/omniauth/strategies/wechat.rb
+++ lib/omniauth/strategies/wechat.rb
@@ -40,7 +40,7 @@ module OmniAuth
         if fix_redirect_uri.present?
           fix_redirect_uri = callback_url.gsub(/http.*profile.auth.wechat/, fix_redirect_uri)
         else
-          fix_redirect_uri = callback_url.gsub(/http.*profile.auth.wechat/, 'https://wechat-staging.letote.cn/profile/auth/wechat')
+          fix_redirect_uri = callback_url
         end
         params = client.auth_code.authorize_params.merge(redirect_uri: fix_redirect_uri).merge(authorize_params)
         params["appid"] = params.delete("client_id")
